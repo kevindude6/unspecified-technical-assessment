7180f4a99b4278337a10f3f2dabd4efa31997338
Kevin D <kevin.dicola@gmail.com>
2025-12-30 22:14:30 +0900

    lint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "@/components/ui/sonner";
 
+// biome-ignore lint/style/noNonNullAssertion: its the root element
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
