5cab7718aa5616cd5c4a8be81799921fdd20a75b
Kevin D <kevin.dicola@gmail.com>
2025-12-30 14:26:40 +0900

    format with biome

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,23 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { prisma } from './lib/prisma.js'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { prisma } from "./lib/prisma.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-app.get('/producttest', async (c) => {
-  const products = await prisma.product.findMany({});
-  return c.json(products, 200);
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+app.get("/producttest", async (c) => {
+	const products = await prisma.product.findMany({});
+	return c.json(products, 200);
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
+);
