5fe052872f10b0c4f7151ee5ccd04fdfb5562ab2
Kevin D <kevin.dicola@gmail.com>
2025-12-30 22:23:16 +0900

    serve built frontend from hono

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,18 +2,32 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import productsRoutes from "./routes/products.js";
 import categoriesRoutes from "./routes/categories.js";
+import { serveStatic } from "@hono/node-server/serve-static";
 
+const publicDir = "./dist/frontend";
 const app = new Hono();
 
 app.use("*", cors());
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
 
 // Nest product routes under /api/products
 app.route("/api/product", productsRoutes);
 
 // Nest category routes under /api/categories
 app.route("/api/category", categoriesRoutes);
+
+app.use(
+	"*",
+	serveStatic({
+		root: publicDir,
+	}),
+);
+
+app.use(
+	"*",
+	serveStatic({
+		root: publicDir,
+		path: "/index.html",
+	}),
+);
 
 export default app;
