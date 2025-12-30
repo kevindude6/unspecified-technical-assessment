a8aac2a342821b0dfe07c5c701ba2b91a0954250
Kevin D <kevin.dicola@gmail.com>
2025-12-30 16:16:37 +0900

    format files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
-
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Button } from './components/ui/button'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Button } from "./components/ui/button";
 
 function App() {
+	const queryClient = new QueryClient();
 
-
-  const queryClient = new QueryClient()
-  
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Button>Click</Button>
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Button>Click</Button>
+		</QueryClientProvider>
+	);
 }
 
-export default App
+export default App;
